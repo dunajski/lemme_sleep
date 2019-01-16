2b9ff28e4664a1db8b6f11e3cd849e23bfeda574
dunajski <michaldunajski2505@gmail.com>
2019-01-16 23:47:19 +0100

    Added types.h for future use

---
+++ types.h
@@ -0,0 +1,16 @@
+/*
+ * types.h
+ *
+ *  Created on: 16 sty 2019
+ *      Author: Dunajski
+ */
+
+#ifndef TYPES_H_
+#define TYPES_H_
+
+#define BV(bit) (1 << bit)
+#define SET_BIT(byte, bit) (byte |= BV(bit))
+#define CLEAR_BIT(byte, bit) (byte &= ~BV(bit))
+#define TOGGLE_BIT(byte, bit) (byte ^= BV(bit))
+
+#endif /* TYPES_H_ */
